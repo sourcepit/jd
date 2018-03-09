7948d92819d619a3f1460110277270982e1c18da
Bernd Vogt <bernd.vogt@sourcepit.org>
2018-03-09 18:01:57 +0100

    Make serializable for usage in ErrorResponseException

--- jd-client-v1.26/src/main/java/org/sourcepit/jd/client/model/ErrorResponse.java
+++ jd-client-v1.26/src/main/java/org/sourcepit/jd/client/model/ErrorResponse.java
@@ -1,12 +1,16 @@
 package org.sourcepit.jd.client.model;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
 @Data
-public class ErrorResponse
+public class ErrorResponse implements Serializable
 {
+	private static final long serialVersionUID = 1L;
+
 	@JsonProperty(required = true)
 	private String message;
 }
